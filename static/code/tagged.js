10412844261abf222bb8949bfb22bb5c35e517bf
James Goodrich <jdgoodrichjr@gmail.com>
2019-04-11 12:20:06 -0500

    Adds the display of tags to their respective groups.
    The "Methodologies" section was renamed on a static variant of my one-page
    resume to "Processes", and this is now incorporated into the interactive
    version.
    
    There's quite a bit of styling to decide and apply, but for now, the desired
    functionality is available. In the future, the tag data won't be coming from
    tags.json, but from experience.json and educations.json.

--- static/code/tagged.js
+++ static/code/tagged.js
@@ -18,6 +18,11 @@ function tagGroupId(tagEntry)
     return "tag-group-" + getTagGroup(tagEntry).toLowerCase();
 }
 
+function tagGroupListId(tagEntry)
+{
+    return tagGroupId(tagEntry) + "-list";
+}
+
 function createTagGroup(tagEntry)
 {
     var group = sectionNode();
@@ -27,13 +32,13 @@ function createTagGroup(tagEntry)
     group.appendChild(groupHeading);
 
     var groupListing = unorderedListNode();
-    setId(groupListing, tagGroupId(tagEntry) + "-list");
+    setId(groupListing, tagGroupListId(tagEntry));
     group.appendChild(groupListing);
 
     return group;
 }
 
-function getOrCreateTagGroup(tagEntry)
+function ensureTagGroupExists(tagEntry)
 {
     var groupId = tagGroupId(tagEntry);
 
@@ -48,13 +53,51 @@ function getOrCreateTagGroup(tagEntry)
 
         tagGroups.appendChild(tagGroup);
     }
+}
 
-    return tagGroup;
+function getTagValue(tagEntry)
+{
+    with (tagEntry)
+    {
+        if ("value" in tagEntry)
+        {
+            return value;
+        }
+        else
+        {
+            return "Unknown";
+        }
+    }
+}
+
+function tagListItemId(tagEntry)
+{
+    var group = getTagGroup(tagEntry).toLowerCase();
+    var value = getTagValue(tagEntry).toLowerCase();
+
+    return "tag-" + group + "-" + value;
 }
 
 function addTag(tagEntry)
 {
-    var tagGroup = getOrCreateTagGroup(tagEntry);
+    // Make sure there is a tag group waiting to receive the tag.
+    ensureTagGroupExists(tagEntry);
+
+    // Now we can add the tag as a list item.
+    var listItemId = tagListItemId(tagEntry);
+
+    var tagListItem = document.getElementById(listItemId);
+
+    // But only add the tag if the list item doesn't exist already.
+    if (!tagListItem)
+    {
+        tagListItem = listItemNode();
+
+        tagListItem.appendChild(textNode(getTagValue(tagEntry)));
+        setId(tagListItem, listItemId);
+
+        document.getElementById(tagGroupListId(tagEntry)).appendChild(tagListItem);
+    }
 }
 
 function processJsonTagData(tagData)
